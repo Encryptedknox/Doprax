82a043d3434457e8cbebc8c456c31937a26ba8ab
knox <111348305+aceknox@users.noreply.github.com>
2023-01-30 19:33:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone $REPO_URL Bot && apt update && apt upgrade && sudo su && cd Bot && pip3 install -U -r requirements.txt
+git clone $REPO_URL pro && apt update && apt upgrade && sudo su && cd pro && pip3 install -U -r requirements.txt
 $START_CMD
