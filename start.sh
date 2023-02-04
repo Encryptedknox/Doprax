c2cbb84c7d464a6fa2ee8d40ad4a27dd3bfe2291
knox <111348305+aceknox@users.noreply.github.com>
2023-02-04 08:18:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone $REPO_URL tgurl && apt update && cd tgurl && pip3 install -U -r requirements.txt
+git clone $GIT_CLONE tgurl && apt update && cd tgurl && pip3 install -U -r requirements.txt
 $START_CMD
