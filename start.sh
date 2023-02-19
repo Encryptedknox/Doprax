3df50daa1ee5628e1184a8daeb31c5d807ff104a
knox <111348305+aceknox@users.noreply.github.com>
2023-02-19 16:02:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone $GIT t && apt update && cd t && pip3 install -U -r requirements.txt
+git clone $GIT t && apt update && cd t && apt install git python3-pip ffmpeg -y && pip3 install -U -r requirements.txt
 $CMD
