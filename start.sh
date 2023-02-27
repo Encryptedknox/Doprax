a758cf362f9b91b5a321899c9f00f39bc2847358
knox <111348305+aceknox@users.noreply.github.com>
2023-02-27 16:49:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone $GIT gra && apt update && cd t && pip3 install -U -r requirements.txt
+git clone $GIT theban && apt update && cd theban && pip3 install -U -r requirements.txt
 $CMD
